822a3f3c971eb038a01175f9bae1d994f1610774
Daniel Brockman <daniel@brockman.se>
2016-11-10 16:35:38 +0100

    add support for confirming and triggering actions

---
+++ Makefile
@@ -0,0 +1,1 @@
+server:; python -m SimpleHTTPServer
--- index.js
+++ index.js
@@ -10,10 +10,15 @@ if (address) {
 }
 
 var DSEasyMultisig = abi([
-  ["getInfo", [], ["uint", "uint", "uint", "uint"]],
-  ["getActionStatus", ["uint256"], [
-    "uint", "uint", "bool", "address", "uint"
+  ["isMember", ["address"], ["bool"]],
+  ["getInfo", [], [
+    "uint256", "uint256", "uint256", "uint256"
   ]],
+  ["getActionStatus", ["uint256"], [
+    "uint256", "uint256", "bool", "address", "uint256"
+  ]],
+  ["confirm", ["address", "uint256"], []],
+  ["trigger", ["address", "uint256"], []],
 ])
 
 var loading = setTimeout(function() {
@@ -43,20 +48,25 @@ onload = function() {
 
 function load() {
   multisig = web3.eth.contract(DSEasyMultisig).at(address)
-  multisig.getInfo.call(hopefully(info => {
-    quorum = Number(info[0])
-    admins = Number(info[1])
-    window = Number(info[2])
-    async.times(Number(info[3]), (i, callback) => {
-      getAction(i + 1, callback)
-    }, hopefully(xs => {
-      actions = xs.reverse()
-      render()
+  async.filter(web3.eth.accounts, (x, callback) => {
+    multisig.isMember.call(x, hopefully(callback))
+  }, hopefully(xs => {
+    account = xs[0]
+    multisig.getInfo.call(hopefully(info => {
+      quorum = Number(info[0])
+      admins = Number(info[1])
+      window = Number(info[2])
+      async.times(Number(info[3]), (i, callback) => {
+        loadAction(i + 1, callback)
+      }, hopefully(xs => {
+        actions = xs.reverse()
+        render()
+      }))
     }))
   }))
 }
 
-function getAction(id, callback) {
+function loadAction(id, callback) {
   multisig.getActionStatus.call(id, hopefully(status => {
     var confirmations = Number(status[0])
     var expiration = new Date(Number(status[1]) * 1000)
@@ -65,16 +75,33 @@ function getAction(id, callback) {
     var confirmed = confirmations >= quorum
     var status = triggered ? "Triggered" : expired ? "Expired"
       : confirmed ? "Untriggered" : "Unconfirmed"
+    var actionable = status == "Unconfirmed" || status == "Untriggered"
     callback(null, {
-      id, confirmations, expiration,
-      expired, triggered, status
+      id, confirmations, expiration, expired,
+      triggered, status, actionable,
     })
   }))
+}
+
+function getAction(id) {
+  return actions.filter(x => x.id == id)[0]
 }
 
 function render() {
   clearTimeout(loading)
   app.innerHTML = `
+    <div class=info>
+      ${account ? `
+        <div class=note>
+          <h2>You are a member of this multisig:</h2>
+          <code class=address>${account}</code>
+        </div>
+      ` : `
+        <div class=note>
+          You are not a member of this multisig.
+        </div>
+      `}
+    </div>
     <table>
       <tr>
         <th>Action</th>
@@ -83,17 +110,44 @@ function render() {
         <th>Status</th>
       </tr>
       ${actions.map(({
-        id, confirmations, triggered, expired, expiration, status
+        id, confirmations, expiration, expired,
+        triggered, status, actionable,
       }) => `
         <tr class="${classes({ expired, triggered })}">
           <td>${id}</td>
           <td>${confirmations}/${admins} (need ${quorum})</td>
           <td>${moment(expiration).fromNow()}</td>
-          <td class=small-caps>${status}</td>
+          <td data-action=${id}>
+            ${actionable && account ? `
+              <a href=# onclick="
+                takeAction(${id})
+                event.preventDefault()
+              ">${status}</a>
+            ` : status}
+          </td>
         </tr>
       `).join("")}
     </table>
   `
+}
+
+function takeAction(id) {
+  var status = getAction(id).status
+  if (status == "Unconfirmed") {
+    if (confirm(`Are you sure you want to confirm action #${id}?`)) {
+      send(multisig.confirm, id)
+    }
+  } else if (status == "Untriggered") {
+    if (confirm(`Are you sure you want to trigger action #${id}?`)) {
+      send(multisig.trigger, id)
+    }
+  }
+}
+
+function send(method, ...args) {
+  method.send(...args, { from: account }, hopefully(tx => {
+    alert(`Transaction created: ${tx}`)
+  }))
 }
 
 function classes(names) {
